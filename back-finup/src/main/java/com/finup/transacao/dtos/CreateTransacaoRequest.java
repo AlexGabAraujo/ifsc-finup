3e5fc21c6687102e1be53dc72097eaf7e41e7eb0
Alex Araujo <alexgabriel.araujo@hotmail.com>
2026-03-18 17:30:23 -0300

    criando dto para criar transacao

---
+++ back-finup/src/main/java/com/finup/transacao/dtos/CreateTransacaoRequest.java
@@ -0,0 +1,29 @@
+package com.finup.transacao.dtos;
+
+import com.finup.pessoaFisica.PessoaFisica;
+import com.finup.transacao.TipoGasto;
+import com.finup.transacao.TipoPagamento;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record CreateTransacaoRequest(
+        @NotNull(message = "É obrigatório informar o valor da transação.")
+        BigDecimal valor,
+
+        @NotNull(message = "É obrigatório associar uma transação a uma pessoa.")
+        Long pessoaFisicaId,
+
+        @NotNull(message = "É obrigatório informar o tipo de pagamento.")
+        TipoPagamento tipoPagamento,
+
+        @NotNull(message = "É obrigatório informar o tipo de gasto.")
+        TipoGasto tipoGasto,
+
+        Long subClasse,
+        ClassePrincipal classePrincipal,
+        Cnpj cnpj
+) {
+}
