e0e3c18f21ab0bc47e1ccee153c82b49383b58c0
Alex Araujo <alexgabriel.araujo@hotmail.com>
2026-03-21 23:24:28 -0300

    iniciando implementação do telegram e adicionando .env

--- back-finup/src/main/java/com/finup/FinUpApplication.java
+++ back-finup/src/main/java/com/finup/FinUpApplication.java
@@ -1,13 +1,22 @@
 package com.finup;
 
+import com.finup.telegramBot.Bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class FinUpApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws TelegramApiException {
+
 		SpringApplication.run(FinUpApplication.class, args);
+
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        Bot bot = new Bot();
+        telegramBotsApi.registerBot(bot);
 	}
 
 }
---
+++ back-finup/src/main/java/com/finup/telegramBot/Bot.java
@@ -0,0 +1,50 @@
+package com.finup.telegramBot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return "FinUp_IfscBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update == null || !update.hasMessage())
+            return;
+
+        var message = update.getMessage();
+
+        if (message.hasText())
+            System.out.println("Texto recebido: " + message.getText());
+
+        if (message.hasVoice())
+            System.out.println("Voice recebida: " + message.getVoice().getFileId());
+
+        if (message.hasPhoto())
+            System.out.println("Foto recebida: " + message.getPhoto().get(0).getFileId());
+
+        var chatId = message.getChatId();
+
+        var sendMassage = SendMessage.builder().chatId(chatId.toString()).text("Olá, eu sou o FinUp Bot, o que você deseja?").build();
+
+        try{
+            execute(sendMassage);
+        } catch (TelegramApiException e) {
+            System.out.println("Mensagem da api do telegram: " + e.getMessage());
+        }
+    }
+}
